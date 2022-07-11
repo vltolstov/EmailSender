6128f1197f3e34dc3578a4f4fd1454858ca54890
vltolstov <tolstov@gmail.com>
2022-07-11 14:50:42 +0700

    Подключена таблица config и базовый шаблон

---
+++ Mailer/resources/views/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+    <base href="{{$baseUrl}}">
+    <title>Mailer</title>
+
+    <link rel="stylesheet" type="text/css" href="/css/app.css" />
+
+    <link rel="shortcut icon" href="/favicon.ico" />
+</head>
+<body>
+
+тут
+
+</body>
+</html>
