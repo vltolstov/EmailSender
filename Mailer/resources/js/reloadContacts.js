1ad384c7d5b248e347beb39e067963df75f1310c
vltolstov <tolstov@gmail.com>
2022-07-19 17:05:46 +0700

    Добавлена и настроена кнопка обновления контактов

--- Mailer/resources/js/reloadContacts.js
+++ Mailer/resources/js/reloadContacts.js
@@ -4,14 +4,21 @@
 function reloadContacts()
 {
 
-    let xhttp = new XMLHttpRequest();
-    let token = document.querySelector('.token');
+    let icon = document.querySelector('.reloadContacts');
+    icon.classList.remove('reload');
+    icon.classList.add('loading');
 
+    setTimeout(1000);
+
+    let xhttp = new XMLHttpRequest();
     xhttp.open('GET', 'http://127.0.0.1:8000/test', true);
-    xhttp.setRequestHeader('X-CSRF-Token', token.value);
     xhttp.onreadystatechange = function () {
         if(this.readyState == 4 && this.status == '200') {
+            icon.classList.remove('loading');
+            icon.classList.add('complete');
+
             console.log(this.responseText);
+
         }
     };
     xhttp.send();
--- Mailer/resources/views/sidebar.blade.php
+++ Mailer/resources/views/sidebar.blade.php
@@ -1,11 +1,14 @@
 <ul class="sidebar">
     <li><a href="{{route('addressbooks.index')}}">Адресные книги</a></li>
-    <li><a href="{{route('contacts')}}">Контакты</a></li>
+    <li>
+        <a href="{{route('contacts')}}">Контакты</a>
+        <form method="get">
+            <input type="hidden" name="_token" class="token" value="{{{ csrf_token() }}}">
+            <button type="button" class="reloadContacts reload"></button>
+        </form>
+    </li>
 </ul>
 
-<form method="get">
-    <input type="hidden" name="_token" class="token" value="{{{ csrf_token() }}}">
-    <button type="button" class="reloadContacts">Обновить</button>
-</form>
+
 
 <div class="reloadContactsOut"></div>
