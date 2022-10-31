3e40a9681393849971d7b75ceaa46253d870ce3d
vltolstov <tolstov@gmail.com>
2022-10-31 14:31:27 +0700

    Добавлена кнопка выхода

--- Mailer/resources/views/sidebar.blade.php
+++ Mailer/resources/views/sidebar.blade.php
@@ -3,15 +3,9 @@
     <li>
         <a class="sidebarContacts" href="{{route('contacts')}}">Контакты</a>
         <span class="reloadContacts reload">&nbsp;</span>
-
-{{--        <form method="get">--}}
-{{--            <input type="hidden" name="_token" class="token" value="{{{ csrf_token() }}}">--}}
-{{--            <button type="button" class="reload"></button>--}}
-{{--        </form>--}}
     </li>
     <li><a href="{{route('blocked')}}">Заблокированные контакты</a></li>
     <li><a href="{{route('mailing-templates.index')}}">Шаблоны</a></li>
     <li><a href="{{route('mailing-lists.index')}}">Рассылки</a></li>
+    <li><a href="{{route('logout')}}">Выход</a></li>
 </ul>
-
-<div class="reloadContactsOut"></div>
